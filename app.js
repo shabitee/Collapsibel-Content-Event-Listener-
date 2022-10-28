7fa9f56529b2641309c6feed935e504bf389f0ad
T.Sharp <94993051+shabitee@users.noreply.github.com>
2022-10-28 11:49:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+const button = document.querySelector("#button");
+const closeUp = document.querySelector("#button2");
+const text = document.querySelector("p");
+button.addEventListener("click", () => {text.classList.remove("dynamic-content-text")});
+closeUp.addEventListener("click", () => {text.classList.add("dynamic-content-text")});
